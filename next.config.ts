c2788927681040e85e6b27e7e6f43ec78ea5687e
xorms571 <xorms6865@naver.com>
2024-10-25 15:58:32 +0900

    img

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     domains: ['shopping-phinf.pstatic.net', 'api-project-su1v-6b8h9h873-bigroots-projects-4b5184a8.vercel.app'],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
